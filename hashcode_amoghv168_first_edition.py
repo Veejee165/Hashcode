b27623598082701b5bf20378734a8e22cffaa11f
Rahul-s-007 <70894220+Rahul-s-007@users.noreply.github.com>
2022-02-25 03:06:18 +0530

    Update hashcode_amoghv168_first_edition.py

--- hashcode_amoghv168_first_edition.py
+++ hashcode_amoghv168_first_edition.py
@@ -3,6 +3,7 @@ class Contributor:
         self.name = name
         self.skill_count = skill_count
         self.skill_dict = {}
+        self.occupied = False
 
     def add_skill(self, skill, L):
         self.skill_dict[skill] = L
@@ -27,6 +28,7 @@ class Project:
 
 
 f = open("a_an_example.in.txt", "r")
+
 inp1 = f.readline()
 inp = [int(x) for x in inp1.split()]
 C = inp[0]
@@ -46,17 +48,47 @@ for c in range(C):
         contributors[cname].add_skill(skill, L)
 
 
-project_skill = {}
+projects = {}
 for c in range(P):
     inp1 = f.readline().split()
     pname, D, S, B, R = inp1[0], int(inp1[1]), int(inp1[2]), int(inp1[3]), int(inp1[4])
     #print(pname, D, S, B, R)
-    project_skill[pname] = Project(pname, D, S, B, R)
+    projects[pname] = Project(pname, D, S, B, R)
     for n in range(R):
         inp1 = f.readline().split()
         skill, L = inp1[0], int(inp1[1])
-        project_skill[pname].add_req_skill(skill, L)
+        projects[pname].add_req_skill(skill, L)
 
+ans = []
+num_pr=0
+for project in projects.values():
+    proj_completed = False
+    temp = []
+    completed_roles = []
+    for role in project.skill_dict.keys():
+        completed = False
+        for contributor in contributors.values():
+            if not contributor.occupied:
+                if role in contributor.skill_dict.keys():
+                    #print(contributor.skill_dict[role], project.skill_dict[role])
+                    if contributor.skill_dict[role] >= project.skill_dict[role]:
+                        temp.append(contributor.name)
+                        completed = True
+                        contributor.occupied = True
+                if completed:
+                    completed_roles.append(role)
+                    break
+    if len(completed_roles) == project.R:
+        proj_completed = True
+        num_pr +=1
+        ans_name = ""
+        for i in range(len(temp)):
+            ans_name = ans_name+temp[i]+" "
+        ans=[project.name,ans_name]
+
+print(num_pr)
+for i in range(len(ans)):
+            print(ans[i])
 
 """
 contributors[name].lvl_up_skill(skill)
