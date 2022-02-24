3f445d3f7362268f4dbc33df8e05186988cad20c
Rahul-s-007 <70894220+Rahul-s-007@users.noreply.github.com>
2022-02-25 00:23:40 +0530

    Update hashcode_amoghv168_first_edition.py

--- hashcode_amoghv168_first_edition.py
+++ hashcode_amoghv168_first_edition.py
@@ -2,8 +2,13 @@ class Contributor:
     def __init__(self, name, skill_count):
         self.name = name
         self.skill_count = skill_count
-C = int(input())
-P = int(input())
+
+f = open("C:\\Users\\RAHUL\\Desktop\\HashCode\\data\\a_an_example.in.txt", "r")
+inp1 = f.readline()
+inp = [int(x) for x in inp1.split()]
+C = inp[0]
+P = inp[1]
+
 contributors = {}
 for c in range(C):
     name, skill_count = input().split()
