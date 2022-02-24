64263a56a9e5867f69180a42785cf7d938913442
Rahul-s-007 <70894220+Rahul-s-007@users.noreply.github.com>
2022-02-25 00:47:26 +0530

    Update hashcode_amoghv168_first_edition.py

--- hashcode_amoghv168_first_edition.py
+++ hashcode_amoghv168_first_edition.py
@@ -12,10 +12,23 @@ inp = [int(x) for x in inp1.split()]
 C = inp[0]
 P = inp[1]
 
+print(C,P)
+
 contributors = {}
 for c in range(C):
-    name, N = input().split()
+    inp1 = f.readline().split()
+    name, N = inp1[0], int(inp1[1])
+    #print(name, N)
     contributors[name] = Contributor(name, int(N))
     for n in range(N):
-        skill, L = input().split()
+        inp1 = f.readline().split()
+        skill, L = inp1[0], int(inp1[1])
         contributors[name].add_skill(skill, L)
+        
+"""
+for key, value in contributors.items():
+    print(key, ' : ', value.skill_dict)
+
+for key, value in contributors.items():
+    print(key, ' : ', value.skill_count)
+"""
